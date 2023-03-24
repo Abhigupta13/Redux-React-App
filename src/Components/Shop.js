76220f755bdbbb15d9711333e517fb2194235188
Abhishek <abhishekkumargupta22110@gmail.com>
2023-03-24 18:08:57 +0530

    CREATE state component

---
+++ src/Components/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div className='text-center my-1'>
+        <h1>Deposit / withdrow Money</h1>
+         <button className="btn btn-primary mx-2">+</button>
+        <b>Update money</b>
+        <button className="btn btn-primary mx-2">-</button>
+    </div>
+  )
+}
+
+export default Shop
